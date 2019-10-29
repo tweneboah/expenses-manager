67cdf44168ab97b0395d1c5461ad6f922356399c
Emmanuel Tweneboah <twentekghana@gmail.com>
2019-10-30 00:04:46 +0800

    Homepage designed

--- client/src/Components/Header/index.js
+++ client/src/Components/Header/index.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
+
 class Header extends Component {
 
     render() {
         return (
-            <div>
-                <div><Link to='/'>Home</Link></div>
-                <Link to='/list'>List of Expenses</Link>
-                <div><Link to='/create'>Create</Link></div>
-                <div><Link to='/dashboard'>Dashboard</Link></div>
+            <div >
+                <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+                    <div class="container">
+                        <Link class="navbar-brand" to="/">Hicotek-Xpensify</Link>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarResponsive">
+                            <ul class="navbar-nav ml-auto">
+                                <li class="nav-item active">
+                                    <Link class="nav-link" to="/">Home
+                <span class="sr-only">(current)</span>
+                                    </Link>
+                                </li>
+                                <li class="nav-item">
+                                    <Link class="nav-link" to="/dashboard">Dashboard</Link>
+                                </li>
+                                <li class="nav-item">
+                                    <Link class="nav-link" to="/create">Create expenses</Link>
+                                </li>
+
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
             </div>
+
+
         );
     }
 }
--- client/src/Components/Landing/index.js
+++ client/src/Components/Landing/index.js
@@ -1,11 +1,87 @@
 import React from 'react';
+import '../../assets/css/style.css'
+import '../../assets/lib/bootstrap/css/bootstrap.min.css';
+import '../../assets/lib/font-awesome/css/font-awesome.css';
 
 const Landing = (props) => {
     return (
 
         <div>
-            <h1>Welcome to Xpensify-App</h1>
-            <p>Developed by HicoTek and Akaris</p>
+
+            <section id="hero">
+                <div class="hero-container">
+                    <h1>Welcome To Xpensify</h1>
+                    <h2>Get to know what is happening in your Business everywhere you are</h2>
+                    <a href="/create" class="btn-get-started">Get Started</a>
+                </div>
+            </section>
+
+            <section id="about">
+                <div class="container">
+                    <div class="row about-container">
+
+                        <div class="col-lg-6 content order-lg-1 order-2">
+                            <h2 class="title">Welcome  Xpensify App</h2>
+                            <hr />
+                            <p>
+                                With Xpensify app we help to increase productivity and efficiency of your Business. As a Business owner, you maybe away from your company or you may have different branches of your company so to be able to know what's happening in your company you have to make series of calls but with Xpensify you can get an update, daily report and others with ease
+                            </p>
+
+                            <div class="icon-box wow fadeInUp">
+                                <div class="icon"><i class="fa fa-shopping-bag"></i></div>
+                                <h4 class="title"><a href="">Security</a></h4>
+                                <p class="description">All your input records are secure under our server</p>
+                            </div>
+
+                            <div class="icon-box wow fadeInUp" data-wow-delay="0.2s">
+                                <div class="icon"><i class="fa fa-photo"></i></div>
+                                <h4 class="title"><a href="">Performance</a></h4>
+                                <p class="description">We develop software with modern programming languages with efficient testing to deployment hence it has high performance</p>
+                            </div>
+
+
+
+                        </div>
+
+                        <div class="col-lg-6 background order-lg-2 order-1 wow fadeInRight"></div>
+                    </div>
+
+                </div>
+            </section>
+
+
+            <section id="services">
+                <div class="container wow fadeIn">
+                    <div class="section-header">
+                        <h3 class="section-title">What we can do for you</h3>
+                        <p class="section-description">we help you grow</p>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.2s">
+                            <div class="box">
+                                <div class="icon"><a href=""><i class="fa fa-desktop"></i></a></div>
+                                <h4 class="title"><a href="">Daily Sales Report</a></h4>
+                                <p class="description">With Xpensify will be able to get report on daily sales without hasle</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.4s">
+                            <div class="box">
+                                <div class="icon"><a href=""><i class="fa fa-bar-chart"></i></a></div>
+                                <h4 class="title"><a href="">Expenses</a></h4>
+                                <p class="description">Managing Expenses somestimes difficult but with Xpensify we help you to keep track of your expenses</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.6s">
+                            <div class="box">
+                                <div class="icon"><a href=""><i class="fa fa-paper-plane"></i></a></div>
+                                <h4 class="title"><a href="">Workers Report</a></h4>
+                                <p class="description">With this reporting system the Business owner will know who is not rendering his/her responsibilities</p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </section>
         </div>
     );
 };
